a756e814f262dbaca937aeee5248728c7e7d5806
Diewaay <muhammadhf91@gmail.com>
2025-02-22 16:26:26 +0700

    add components sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { BiSearch } from "react-icons/bi";
+import { HiHome } from "react-icons/hi";
+import Box from "./Box";
+import SIdebarItem from "./SIdebarItem";
+
+interface SidebarProps {
+  children: React.ReactNode;
+}
+const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const pathName = usePathname();
+  const routes = useMemo(
+    () => [
+      {
+        icon: HiHome,
+        label: "Home",
+        active: pathName !== "/search",
+        href: "/",
+      },
+      {
+        icon: BiSearch,
+        label: "Search",
+        active: pathName === "/search",
+        href: "/search",
+      },
+    ],
+    [pathName]
+  );
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box>
+          <div className="flex flex-col gap-y-4 px-5 py-4">
+            {routes.map((item) => (
+              <SIdebarItem key={item.label} {...item} />
+            ))}
+          </div>
+        </Box>
+        <Box className="overflow-y-auto h-full">Song Library</Box>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
