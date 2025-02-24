c08e5795941ce65bb19aad45239ebd50c5a0dfe4
Diewaay <muhammadhf91@gmail.com>
2025-02-24 15:53:17 +0700

    add main content #1

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps {
@@ -10,13 +10,16 @@ interface ListItemProps {
   href: string;
 }
 const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
-  //const router = useRouter();
+  const router = useRouter();
   const onClick = () => {
     // add authentication before push
     router.push(href);
   };
   return (
-    <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+    <button
+      onClick={onClick}
+      className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
+    >
       <div className="relative min-h-[64px] min-w-[64px]">
         <Image className="object-cover" fill src={image} alt="Image" />
       </div>
