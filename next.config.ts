27e9ca05448d1c403de3aef6b59d4a89ba33fd79
Diewaay <muhammadhf91@gmail.com>
2025-03-07 13:44:58 +0700

    add song interface

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["prhisyacrukdjwkysdho.supabase.co"],
+  },
 };
 
 export default nextConfig;
